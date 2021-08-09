a55299672dbae78a5226b01322e9f974b0558e1a
Enes Igneci <enes.igneci@mobven.com>
2021-08-09 17:44:02 +0300

    variable usage changed

--- app/src/main/java/com/mobven/extensions/Menu.kt
+++ app/src/main/java/com/mobven/extensions/Menu.kt
@@ -1,6 +1,6 @@
 package com.mobven.extensions;
 
-enum class Menu(var value: String) {
+enum class Menu(val value: String) {
     SINGLE_SELECT_LIST("Single Selectable RecyclerView"),
     VIEW_EXT("View Ext.")
 }
